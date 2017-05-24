d75ed32ec23d66ab4e7a097c13ed70a5f48b2112
Henrique Breim <henrique@breim.com.br>
2017-05-24 02:39:56 -0300

    move to final to prevent any subscribed route rule

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 	root 'pages#index'
-	get '/:token' => 'links#show'
 	devise_for :users
 	resources :links, only: :index
 	resources :credentials, only: %i(index update)
+	get '/:token' => 'links#show'
 
 	namespace :api do
 		resources :links, only: %i(index show create update destroy)
