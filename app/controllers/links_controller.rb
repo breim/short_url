c7634d410aee5b8d748006a963e15aa50ef3f8fd
Henrique Breim <henrique@breim.com.br>
2017-05-23 22:50:17 -0300

    add pagination to links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,10 @@
 class LinksController < ApplicationController
   before_action :authenticate_user!
+  respond_to :html
+
   def index
-  	@links = Link.where(user_id: current_user.id).order(created_at: :desc)
+    params[:page] = 1 unless params[:page].present?
+    @links = Link.where(user_id: current_user.id).order(created_at: :desc).offset((params[:page].to_i-1) * 30).limit(30)
+    respond_with(@links)
   end
 end
