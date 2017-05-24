dd6da8c822340b72fafa8165b7aa014620da06d3
Henrique Breim <henrique@breim.com.br>
2017-05-23 23:29:16 -0300

    automatic generate credentials key and pwd

--- app/controllers/credentials_controller.rb
+++ app/controllers/credentials_controller.rb
@@ -1,7 +1,13 @@
 class CredentialsController < ApplicationController
+  before_action :authenticate_user!
+  respond_to :html
+  
   def index
+  	current_user.update(key: SecureRandom.hex(5), pwd: SecureRandom.hex(5)) if current_user.key.nil?
   end
 
   def update
+  	current_user.update(key: SecureRandom.hex(5), pwd: SecureRandom.hex(5))
+  	redirect_to credentials_path
   end
 end
