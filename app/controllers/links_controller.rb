8d8148890302a28a8a7f28e7a415f8905e88a494
Henrique Breim <henrique@breim.com.br>
2017-05-23 21:45:02 -0300

    add links table and controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,8 +1,6 @@
 class LinksController < ApplicationController
   before_action :authenticate_user!
   def index
-  end
-
-  def show
+  	@links = Link.where(user_id: current_user.id).order(created_at: :desc)
   end
 end
