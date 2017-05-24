c5fbefc281b937a15094daad5e0383205301db05
Henrique Breim <henrique@breim.com.br>
2017-05-23 21:40:37 -0300

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-  get 'links/index'
-
-  get 'links/show'
-
 	root 'pages#index'
-
 	devise_for :users
+	resources :links, only: :index
 
 	namespace :api do
 		resources :links, only: %i(index show create update destroy)
 	end
-	
 end
