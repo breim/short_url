f53e94119e669dafcd009c0f71cad137eba97b93
Henrique Breim <henrique@breim.com.br>
2017-05-23 21:40:27 -0300

    add redirection after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   helper_method :current_user
+
+private
+  def after_sign_in_path_for(resource_or_scope)
+    links_path
+  end
 end
