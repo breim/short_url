376c7f021e9db6bf5db1d0d6c04f344b0b300595
Henrique Breim <henrique@breim.com.br>
2018-06-05 16:16:17 -0300

    test

--- app/controllers/api/trackings_controller.rb
+++ app/controllers/api/trackings_controller.rb
@@ -2,11 +2,8 @@ class Api::TrackingsController < Api::ApiController
   def show
     @link = Link.find_by_token(params[:id])
     @tracking = Tracking.find_by_link_id(@link.id)
-    @objs = {}
-    @objs << @link
-    @objs << @tracking
 
-    render json: @objs
+    render json: @tracking, @link
   end
 
 end
