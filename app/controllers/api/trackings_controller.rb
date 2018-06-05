3903658de66b4eb401d5121f3d06997961a7f7cf
Henrique Breim <henrique@breim.com.br>
2018-06-05 16:15:06 -0300

    remove before action

--- app/controllers/api/trackings_controller.rb
+++ app/controllers/api/trackings_controller.rb
@@ -1,6 +1,4 @@
 class Api::TrackingsController < Api::ApiController
-  before_action :set_link, only: [:show, :update, :destroy]
-
   def show
     @link = Link.find_by_token(params[:id])
     @tracking = Tracking.find_by_link_id(@link.id)
