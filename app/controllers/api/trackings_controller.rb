76ff90232c21d2ee7a0d0cbcf0d62f72762df67c
Henrique Breim <henrique@breim.com.br>
2018-06-05 16:20:41 -0300

    fix order

--- app/controllers/api/trackings_controller.rb
+++ app/controllers/api/trackings_controller.rb
@@ -1,7 +1,7 @@
 class Api::TrackingsController < Api::ApiController
   def show
     @link = Link.find_by_token(params[:id])
-    @tracking = Tracking.find_by_link_id(@link.id)
-    render json: [@tracking, @link]
+    @tracking = Tracking.where(link_id: @link.id)
+    render json: [@link, @tracking]
   end
 end
