66704c4f186c378de156785d13e8d177fbe2f86d
Henrique Breim <henrique@breim.com.br>
2018-06-05 16:18:26 -0300

    new opts

--- app/controllers/api/trackings_controller.rb
+++ app/controllers/api/trackings_controller.rb
@@ -2,8 +2,6 @@ class Api::TrackingsController < Api::ApiController
   def show
     @link = Link.find_by_token(params[:id])
     @tracking = Tracking.find_by_link_id(@link.id)
-
-    render json: @tracking, @link
+    render json: [@tracking, @link]
   end
-
 end
