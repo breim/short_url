1ef710da1574ddeb716714d4a236ef862c3c07df
Lean <lndr.figueredo@gmail.com>
2018-07-10 00:45:08 -0300

    Rubocop removing offenses from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
+# app/models/user
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  def update_with_password(params={})
+  def update_with_password(params = {})
     if params[:password].blank?
       params.delete(:password)
       params.delete(:password_confirmation) if params[:password_confirmation].blank?
@@ -13,6 +14,6 @@ class User < ApplicationRecord
   end
 
   def empty_key?
-    self.key.nil?
+    key.nil?
   end
 end
