42a241f7a3a3d82954ddf5c5038c135121197dfa
Lean <lndr.figueredo@gmail.com>
2018-07-09 23:47:01 -0300

    RUBOCOP - removing offenses from links_controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,22 +1,32 @@
+# app/controllers/links_controllers
 class LinksController < ApplicationController
-  before_action :authenticate_user!, only: :index
+  before_action %w(authenticate_user! fill_page).map(&:to_sym), only: :index
   before_action :create_tracking, only: :show
 
   respond_to :html
 
   def index
-    params[:page] = 1 unless params[:page].present?
-    @links = Link.where(user_id: current_user.id).order(created_at: :desc).offset((params[:page].to_i-1) * 40).limit(40)
+    @links = Link.where(user_id: current_user.id)
+                 .order(created_at: :desc).offset((params[:page].to_i - 1) * 40)
+                 .limit(40)
     respond_with(@links)
   end
 
   def show
-    unless @link.nil?; redirect_to @link.original_url; else render html: nil, status: :ok; end
+    redirect_to @link.original_url && return unless @link.nil?
+    render html: nil, status: :ok if @link.nil?
   end
 
-private
+  private
+
+  def fill_page
+    params[:page] = 1 unless params[:page].present?
+  end
+
   def create_tracking
     @link = Link.find_by(token: params[:token])
-    Tracking.create(link_id: @link.try(:id), referer: request.referrer, browser: request.user_agent, ip: request.remote_ip, ip_data: Tracking.get_ip_data(request.remote_ip))
+    Tracking.create(link_id: @link.try(:id), referer: request.referrer,
+                    browser: request.user_agent, ip: request.remote_ip,
+                    ip_data: Tracking.get_ip_data(request.remote_ip))
   end
 end
