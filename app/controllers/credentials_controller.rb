6d544a851119c0777014dbbbcc6c92629b0c8739
Lean <lndr.figueredo@gmail.com>
2018-07-10 00:00:13 -0300

    Rubocop - removing all offenses from credentials controller

--- app/controllers/credentials_controller.rb
+++ app/controllers/credentials_controller.rb
@@ -1,13 +1,14 @@
+# app/controllers/credencials_controller
 class CredentialsController < ApplicationController
   before_action :authenticate_user!
   respond_to :html
-  
+
   def index
-  	current_user.update(key: SecureRandom.hex(5), pwd: SecureRandom.hex(5)) if current_user.key.nil?
+    current_user.update(key: SecureRandom.hex(5), pwd: SecureRandom.hex(5)) if current_user.empty_key?
   end
 
   def update
-  	current_user.update(key: SecureRandom.hex(5), pwd: SecureRandom.hex(5))
-  	redirect_to credentials_path
+    current_user.update(key: SecureRandom.hex(5), pwd: SecureRandom.hex(5))
+    redirect_to credentials_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  def update_with_password(params={}) 
-    if params[:password].blank? 
-      params.delete(:password) 
-      params.delete(:password_confirmation) if params[:password_confirmation].blank? 
-    end 
-    update_attributes(params) 
+  def update_with_password(params={})
+    if params[:password].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation) if params[:password_confirmation].blank?
+    end
+    update_attributes(params)
   end
-  
+
+  def empty_key?
+    self.key.nil?
+  end
 end
