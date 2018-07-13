7fce3f13a34a1920a1c5a63f0c3f3ceddc53bc64
Lean <lndr.figueredo@gmail.com>
2018-07-13 18:36:52 -0300

    Query isolada em model, removendo da view

--- app/models/tracking.rb
+++ app/models/tracking.rb
@@ -3,6 +3,12 @@ require 'net/http'
 class Tracking < ApplicationRecord
   belongs_to :link, optional: true, counter_cache: true
 
+  def self.query_tracking(user)
+    joins(:link).where(links: {user_id: user.id}, created_at: 7.days.ago..Time.now)
+    .group_by_day('trackings.created_at', format: "%a %d/%m/%y")
+    .count
+  end
+
   def self.get_ip_data(ip)
     uri = URI("http://freegeoip.net/json/#{ip}")
     res = JSON.parse Net::HTTP.get(uri)
