a38fa154a1c635e724d792bb43b514c310dd6256
Guilherme Casimiro <guircasimiro@gmail.com>
2018-07-16 14:07:56 -0300

    finish excel report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -12,18 +12,19 @@ class ReportsController < ApplicationController
   private
 
   def get_clicks_by_filter(link, filter)
-    Tracking.where(created_at >= filter.split(' ')[0].days.ago).count
+    Tracking.where("created_at >= ?",  filter.to_i.days.ago).count
   end
 
   def create_report(filter)
     Axlsx::Package.new do |p|
-      p.workbook.add_worksheet(name: 'Report in the last ' + filter + ' days') do |sheet|
+      p.workbook.add_worksheet(name: 'Report in the last ' + filter) do |sheet|
         sheet.add_row ['Original url', 'Short url', 'Clicks']
         @links.each do |link|
           sheet.add_row [link.original_url, link.short_url, get_clicks_by_filter(link, filter)]
         end
       end
-      p.serialize('report.xlsx')
+      send_data (p.to_stream.read), type: "application/xlsx", filename: "report.xlsx"
+      # p.serialize('report.xlsx')
     end
   end
 end
