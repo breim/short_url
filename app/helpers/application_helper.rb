3daa56797af394128ab2f82bded79df473a65790
Lean <lndr.figueredo@gmail.com>
2018-07-24 11:43:11 -0300

    Resolving rubocop Offenses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 # app/helpers/application_helper
 module ApplicationHelper
   def link_create_account
-    "<li>#{ link_to 'Create account', new_user_registration_path }</li>".html_safe unless current_user
+    "<li>#{link_to 'Create account', new_user_registration_path}</li>".html_safe unless current_user
   end
 end
